fe6909d058b063e0f66b267b3d72cee830491a76
ramosjm <jjramos_2004@hotmail.com>
2019-04-16 09:30:35 +0100

    removed console log from film.js

--- src/components/Film.js
+++ src/components/Film.js
@@ -5,7 +5,6 @@ class Film extends Component{
   render(){
     return(
       <div className = "film">
-      {console.log(this.props.children[3])}
       <a href = {this.props.children[3]}>
         <li>{this.props.children[1]}</li>
       </a>
